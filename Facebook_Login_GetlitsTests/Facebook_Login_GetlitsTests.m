ad77f40afc9b2302aaa71698c9b0d108203c78f1
saerem21 <nemessisdds>
2014-01-21 16:50:52 +0900

    Initial Commit

---
+++ Facebook_Login_GetlitsTests/Facebook_Login_GetlitsTests.m
@@ -0,0 +1,34 @@
+//
+//  Facebook_Login_GetlitsTests.m
+//  Facebook_Login_GetlitsTests
+//
+//  Created by SDT-1 on 2014. 1. 21..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Facebook_Login_GetlitsTests : XCTestCase
+
+@end
+
+@implementation Facebook_Login_GetlitsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
